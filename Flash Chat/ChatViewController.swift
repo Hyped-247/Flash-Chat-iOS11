fd819616280b8c5b6d82ad94c1e71ad729953594
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-11-16 21:47:58 -0800

    making progress on the app.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
-
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    
     
     // Declare instance variables here
 
@@ -25,20 +27,24 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //TODO: Set yourself as the delegate and datasource here:
-        
-        
-        
-        //TODO: Set yourself as the delegate of the text field here:
+     
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        messageTextfield.delegate = self
 
         
         
         //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped()))
+        messageTableView.addGestureRecognizer(tapGesture)
         
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil),
+        forCellReuseIdentifier: "customMessageCell")
+        
+        configureTableView()
         
     }
 
@@ -49,18 +55,40 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+
+        let messageArray = ["first Message", "second Message", "thrid message"]
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        
+        return cell
+        
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return
+//    }
     //TODO: Declare tableViewTapped here:
-    
+    func tableViewTapped(){
+        messageTextfield.endEditing(true)
+    }
     
     
     //TODO: Declare configureTableView here:
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     
@@ -72,12 +100,24 @@ class ChatViewController: UIViewController {
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5){
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
     
     
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5){
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
 
     
     ///////////////////////////////////////////
@@ -90,11 +130,29 @@ class ChatViewController: UIViewController {
     
     
     @IBAction func sendPressed(_ sender: AnyObject) {
+        messageTextfield.endEditing(true)
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
         
-        //TODO: Send the message to Firebase and save it in our database
         
+        let messagesDB = Database.database().reference().child("Messages")
         
+        let messageDic = ["Sender" : Auth.auth().currentUser?.email,
+                          "MessageBody" : messageTextfield.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDic){
+            (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message have been successfully saved")
+                self.messageTextfield
+                
+            }
+        }
+
     }
     
     //TODO: Create the retrieveMessages method here:
@@ -105,9 +163,13 @@ class ChatViewController: UIViewController {
     
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
-        
-        //TODO: Log out the user and send them back to WelcomeViewController
-        
+        do {
+            try Auth.auth().signOut()
+            // This will send the user back to the main page after they logout. 
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print("error, cannot log out.. ")
+        }
         
     }
     
